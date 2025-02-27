126f3ae013396bfebb9c75d9db9720eac1ce884b
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-27 04:23:34 +0400

    access token config

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,3 +1,4 @@
+import { useStore } from "@/store/store";
 import { CustomError } from "@/types/custom-error.type";
 import axios from "axios";
 
@@ -11,16 +12,24 @@ const options = {
 
 const API = axios.create(options);
 
+API.interceptors.request.use((config) => {
+  const access_token = useStore.getState().accessToken;
+  if (access_token) {
+    config.headers["Authorization"] = "Bearer " + access_token;
+  }
+  return config;
+});
+
 API.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
-    const { data, status } = error.response;
+    const { data } = error.response;
 
-    if (data === "Unauthorized" && status === 401) {
-      window.location.href = "/";
-    }
+    // if (data === "Unauthorized" && status === 401) {
+    //   window.location.href = "/";
+    // }
 
     const customError: CustomError = {
       ...error,
